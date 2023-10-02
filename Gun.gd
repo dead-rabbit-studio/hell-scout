c6fe9680bd2c3fc81aac2e46d8061c335ba58a8e
Felipe Castilhos <felipecastilhos@gmail.com>
2023-10-02 19:55:02 -0300

    feat: player its shooting

---
+++ Gun.gd
@@ -0,0 +1,23 @@
+extends Node
+
+signal gun_was_fired(bullet: RigidBody2D)
+
+var speed = 1000
+
+func _ready() -> void:
+	pass 
+
+func _process(delta: float) -> void:
+	pass
+
+func shoot() -> void:
+	var bullet: RigidBody2D = preload('res://bullet.tscn').instantiate()
+	var direction: Vector2 = Vector2(1, 0)
+	var gunfirePosition: Vector2 = get_parent().position + Vector2(50, 0)
+	
+	bullet.linear_velocity = direction.normalized() * speed
+	bullet.global_position = gunfirePosition
+	gun_was_fired.emit(bullet)
+
+func _on_bullet_spawner_timeout() -> void:
+	shoot()	
