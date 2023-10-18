def01d30c3b38ad2e81bfb25081f41c9a0185225
Felipe Castilhos <felipecastilhos@gmail.com>
2023-10-17 22:36:11 -0300

    chore: change game colors to better readbility

--- bullet.gd
+++ bullet.gd
@@ -1,11 +1,13 @@
 extends RigidBody2D
 
-@export var still_alive_time_secs: int = 1
+@export var still_alive_time_secs: float = 0.01
 
 func _ready() -> void:
 	var timer: Timer = Timer.new()
+	timer.autostart = true
 	timer.start(still_alive_time_secs)
-	timer.connect("Timeout", _on_timer_timeout)
+	timer.connect("timeout", _on_timer_timeout)
+	add_child(timer)
 	
 func _on_timer_timeout() -> void:
 	queue_free()
