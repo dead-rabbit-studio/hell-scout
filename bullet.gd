db25554f308cb16eb736ed53cde406777aaeb9f4
Felipe Castilhos <felipecastilhos@gmail.com>
2023-10-20 20:56:13 -0300

    fix: bullet timeout

--- bullet.gd
+++ bullet.gd
@@ -1,13 +1,13 @@
 extends RigidBody2D
 
-@export var still_alive_time_secs: float = 0.01
+@export var still_alive_time_secs: float = 0.2
 
 func _ready() -> void:
 	var timer: Timer = Timer.new()
 	timer.autostart = true
-	timer.start(still_alive_time_secs)
 	timer.connect("timeout", _on_timer_timeout)
 	add_child(timer)
-	
+	timer.start(still_alive_time_secs)
+
 func _on_timer_timeout() -> void:
 	queue_free()
