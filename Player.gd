2a229d409fe2cc0454d549c033826b016203c0bc
Felipe Castilhos <felipecastilhos@gmail.com>
2023-10-21 15:44:26 -0300

    chore: improve animation code script

--- Player.gd
+++ Player.gd
@@ -4,39 +4,47 @@ enum PLAYER_DIRECTION { LEFT, TOP, RIGHT, BOTTOM }
 signal changed_direction(PLAYER_DIRECTION)
 signal player_position_update(Vector2)
 
+@onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+
 @export var SPEED = 400.0
 
-var lastDirection: PLAYER_DIRECTION = PLAYER_DIRECTION.RIGHT
-@onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+var _currentDirectionVector: Vector2 = Vector2.ZERO 
+var _lastDirection: PLAYER_DIRECTION = PLAYER_DIRECTION.RIGHT
 
 func _ready() -> void : 
 	animatedSprite.play("idle")
 
+func _process(delta: float) -> void:
+	animate()
+	update_sprite_direction()
+
 func _physics_process(_delta: float) -> void:
-	var directionVector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var playerDirection = direction_vector_to_player_direction(directionVector)
+	_currentDirectionVector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var playerDirection = map_direction_vector_to_player_direction(_currentDirectionVector)
 
-	if playerDirection != lastDirection:
-		lastDirection = playerDirection
-		changed_direction.emit(lastDirection)
+	if playerDirection != _lastDirection:
+		_lastDirection = playerDirection
+		changed_direction.emit(_lastDirection)
 		
-	velocity = directionVector * SPEED
-
-	if velocity.length() > 0:
-		animatedSprite.play('running')
-	else: 
-		animatedSprite.play('idle')
-		
-	if directionVector.x > 0:
-		animatedSprite.scale.x = 1
-	elif directionVector.x < 0:
-		animatedSprite.scale.x = -1
+	velocity = _currentDirectionVector * SPEED
 		
 	move_and_slide()
 	player_position_update.emit(global_position)
+	
+func animate():
+	if velocity.length() > 0:
+		animatedSprite.play('running')
+	else: 
+		animatedSprite.play('idle')	
+		
+func update_sprite_direction():
+	if _currentDirectionVector.x > 0:
+		animatedSprite.scale.x = 1
+	elif _currentDirectionVector.x < 0:
+		animatedSprite.scale.x = -1
 
-func direction_vector_to_player_direction(directionVector: Vector2) -> PLAYER_DIRECTION: 
-	var playerDirection: PLAYER_DIRECTION = lastDirection;
+func map_direction_vector_to_player_direction(directionVector: Vector2) -> PLAYER_DIRECTION: 
+	var playerDirection: PLAYER_DIRECTION = _lastDirection;
 	
 	if directionVector == Vector2(-1, 0):
 		playerDirection = PLAYER_DIRECTION.LEFT
