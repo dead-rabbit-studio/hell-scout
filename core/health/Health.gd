147681080123669d05ce25f15f911c25e5dca7bb
Felipe Castilhos <felipecastilhos@gmail.com>
2023-10-22 12:03:24 -0300

    feat: health cant be depleted if the character is invincible

--- core/health/Health.gd
+++ core/health/Health.gd
@@ -3,22 +3,24 @@ class_name Health extends Node
 signal depleted
 
 @export var is_alive = true
-@export var max = 100
-@export var current: float = max
+@export var is_mortal = true
+@export var max_health = 100
+@export var current: float = max_health
 	
 func heal(heal_points: float):
 	var health_after_heal = current + heal_points
-	if health_after_heal > max: current = max
+	if health_after_heal > max_health: 
+		current = max_health
 	current += heal_points
 
-func damage(damage: float):
+func damage(damage_points: float):
 	if is_alive:
-		var health_after_hit = current - damage
+		var health_after_hit = current - damage_points
 		if health_after_hit < 0: current = 0
-		else: current -= damage
+		else: current -= damage_points
 		
-		if current == 0:
+		if current == 0 and is_mortal:
 			is_alive = false
 			depleted.emit()
 	
-func revive(): current = max
+func revive(): current = max_health
