495851ddda539c950780d499aaf5c8958d1c3877
Felipe Castilhos <felipecastilhos@gmail.com>
2023-10-22 16:47:05 -0300

    feat: add attack to the player based on its direction

--- player/scouter/Player.gd
+++ player/scouter/Player.gd
@@ -4,8 +4,10 @@ enum PLAYER_DIRECTION { LEFT, TOP, RIGHT, BOTTOM }
 signal changed_direction(PLAYER_DIRECTION)
 signal player_position_update(Vector2)
 
-@onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var health: Health = $Health 
+@onready var attack_area = $AttackArea
+@onready var attack_area_shape: RectangleShape2D = $AttackArea/CollisionShape2D.shape as RectangleShape2D
 
 @export var speed = 400.0
 @export var max_health = 110
@@ -16,7 +18,7 @@ var _lastDirection: PLAYER_DIRECTION = PLAYER_DIRECTION.RIGHT
 func _ready() -> void : 
 	health.max_health = max_health
 	health.is_mortal = false
-	animatedSprite.play("idle")
+	remove_child(attack_area)
 
 func _process(delta: float) -> void:
 	if health.is_alive:
@@ -24,6 +26,12 @@ func _process(delta: float) -> void:
 		print_debug(health.current)
 		_animate()
 		_update_sprite_direction()
+		
+		if Input.is_action_pressed('attack'):
+			_calculate_attack_direction(_lastDirection)
+			add_child(attack_area)
+		else:
+			remove_child(attack_area)
 
 func _physics_process(_delta: float) -> void:
 	if health.is_alive:
@@ -44,17 +52,17 @@ func _move_player():
 func _animate():
 	if  health.is_alive:
 		if velocity.length() > 0:
-			animatedSprite.play('running')
+			animated_sprite.play('running')
 		else: 
-			animatedSprite.play('idle')	
+			animated_sprite.play('idle')	
 	else:
-		animatedSprite.pause()
+		animated_sprite.pause()
 		
 func _update_sprite_direction():
 	if _currentDirectionVector.x > 0:
-		animatedSprite.scale.x = 1
+		animated_sprite.scale.x = 1
 	elif _currentDirectionVector.x < 0:
-		animatedSprite.scale.x = -1
+		animated_sprite.scale.x = -1
 
 func _map_direction_vector_to_player_direction(directionVector: Vector2) -> PLAYER_DIRECTION: 
 	var playerDirection: PLAYER_DIRECTION = _lastDirection;
@@ -70,5 +78,24 @@ func _map_direction_vector_to_player_direction(directionVector: Vector2) -> PLAY
 	
 	return playerDirection; 
 
+func _calculate_attack_direction(player_direction: PLAYER_DIRECTION) -> void:
+	var attack_distance =  (attack_area_shape.extents.x * attack_area_shape.extents.y) / 10
+	
+	match(player_direction):
+		PLAYER_DIRECTION.LEFT:
+			attack_area.global_position = global_position + Vector2(-attack_distance, 0)
+		PLAYER_DIRECTION.TOP:
+			attack_area.global_position = global_position + Vector2(0, -attack_distance)
+		PLAYER_DIRECTION.RIGHT:
+			attack_area.global_position = global_position + Vector2(attack_distance, 0)
+		PLAYER_DIRECTION.BOTTOM:
+			attack_area.global_position = global_position + Vector2(0, attack_distance)	
+			
 func _on_health_depleted():
 	print_debug("player has died")
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	const damage = 10
+	if body.has_node('Health'): 
+		body.health.damage(damage)
+		print_debug("player dealt: " + str(damage))
