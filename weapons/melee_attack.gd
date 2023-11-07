28ec28111f95ef491d117ac7c543e8b7dbc3b3fd
Felipe Castilhos <felipecastilhos@gmail.com>
2023-11-07 08:23:31 -0300

    feat: add a colored area placeholder to represent the melee attack

--- weapons/melee_attack.gd
+++ weapons/melee_attack.gd
@@ -3,27 +3,36 @@ class_name MeleeAttack extends Area2D
 enum ATTACK_DIRECTION { LEFT, TOP, RIGHT, BOTTOM }
 @export var attack_direction: ATTACK_DIRECTION = ATTACK_DIRECTION.RIGHT
 @onready var melee_area_shape: RectangleShape2D = $CollisionShape2D.shape as RectangleShape2D
-
+@onready var attack_vfx_shape: ColorRect  = $AttackVfx
 var player_position: Vector2 = Vector2.ZERO
 
 func attack() -> void:
-	var attack_distance = (melee_area_shape.extents.x * melee_area_shape.extents.y) / 10
-	var melee_attack_vertical_size = Vector2(50, 20)
-	var melee_attack_horizontal_size = Vector2(20, 50)
+	const melee_attack_vertical_size = Vector2(50, 20)
+	const melee_attack_horizontal_size = Vector2(20, 50)
 	
+	var attack_distance = (melee_area_shape.extents.x * melee_area_shape.extents.y) / 10
+
 	match(attack_direction):
 		ATTACK_DIRECTION.LEFT:
-			global_position = player_position + Vector2(-attack_distance, 0)
-			melee_area_shape.size = melee_attack_horizontal_size
+			_set_attack_position(Vector2(-attack_distance, 0))
+			_set_attack_shape(melee_attack_horizontal_size)
 		ATTACK_DIRECTION.TOP:
-			global_position = player_position + Vector2(0, -attack_distance)
-			melee_area_shape.size = melee_attack_vertical_size
+			_set_attack_position(Vector2(0, -attack_distance))
+			_set_attack_shape(melee_attack_vertical_size)
 		ATTACK_DIRECTION.RIGHT:
-			global_position = player_position + Vector2(attack_distance, 0)
-			melee_area_shape.size = melee_attack_horizontal_size
+			_set_attack_position(Vector2(attack_distance, 0))
+			_set_attack_shape(melee_attack_horizontal_size)
 		ATTACK_DIRECTION.BOTTOM:
-			global_position = player_position + Vector2(0, attack_distance)	
-			melee_area_shape.size = melee_attack_vertical_size
+			_set_attack_position(Vector2(0, attack_distance))
+			_set_attack_shape(melee_attack_vertical_size)
+			
+func _set_attack_position(new_position: Vector2) -> void:
+	global_position = player_position + new_position
+	attack_vfx_shape.position = global_position 
+
+func _set_attack_shape(size: Vector2) -> void:
+	melee_area_shape.size = size
+	attack_vfx_shape.size = size 
 
 func _on_player_changed_direction(player_direction):
 	attack_direction = player_direction;
