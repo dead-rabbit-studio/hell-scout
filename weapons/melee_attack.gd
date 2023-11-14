41cfe06607fe57ade2bc99b0a82535ac4909d898
Felipe Castilhos <felipecastilhos@gmail.com>
2023-11-14 18:49:19 -0300

    fix: player attack position

--- weapons/melee_attack.gd
+++ weapons/melee_attack.gd
@@ -3,7 +3,6 @@ class_name MeleeAttack extends Area2D
 enum ATTACK_DIRECTION { LEFT, TOP, RIGHT, BOTTOM }
 @export var attack_direction: ATTACK_DIRECTION = ATTACK_DIRECTION.RIGHT
 @onready var melee_area_shape: RectangleShape2D = $CollisionShape2D.shape as RectangleShape2D
-@onready var attack_vfx_shape: ColorRect  = $AttackVfx
 var player_position: Vector2 = Vector2.ZERO
 
 func attack() -> void:
@@ -28,11 +27,9 @@ func attack() -> void:
 			
 func _set_attack_position(new_position: Vector2) -> void:
 	global_position = player_position + new_position
-	attack_vfx_shape.position = global_position 
 
 func _set_attack_shape(size: Vector2) -> void:
 	melee_area_shape.size = size
-	attack_vfx_shape.size = size 
 
 func _on_player_changed_direction(player_direction):
 	attack_direction = player_direction;
