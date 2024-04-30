2b25442d9f15fe125a6cbc535617b8cf2a3d887a
Felipe Castilhos <felipecastilhos@gmail.com>
2024-04-30 09:37:46 -0300

    feat: add warp zones

---
+++ core/WarpZone.gd
@@ -0,0 +1,30 @@
+class_name WarpZone
+extends Area2D
+
+enum WARP_DIRECTION { WARPABLE_DIRECTION, TOP, RIGHT, BOTTOM, LEFT}
+
+@export var exit_zone: WarpZone 
+@export var exit_zone_direction: WARP_DIRECTION
+
+func _on_body_entered(body: Player) -> void:
+	if exit_zone != null:
+		body.position = exit_zone.position
+	
+	_handle_exit_direction(body)
+
+func _handle_exit_direction(body: Player):
+		print_debug("Exit: " + str(exit_zone_direction))
+		match exit_zone_direction:
+			WARP_DIRECTION.WARPABLE_DIRECTION:
+				pass #do nothing because it keeps the object direction
+			WARP_DIRECTION.TOP:
+				if body._currentDirectionVector.y >= 0:
+					body.animated_sprite.flip_v = false
+			WARP_DIRECTION.RIGHT:
+				if body._currentDirectionVector.x <= 0:
+					body.animated_sprite.flip_h = false
+			WARP_DIRECTION.BOTTOM:
+				body.animated_sprite.flip_v = true
+			WARP_DIRECTION.LEFT:
+				if body._currentDirectionVector.x >= 0:
+					body.animated_sprite.flip_h = true
