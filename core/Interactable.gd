1adba7886ec784a5005ee690a4845acc42384eea
Felipe Castilhos <felipecastilhos@gmail.com>
2024-08-31 15:44:11 -0300

    chore: remove unused spritesheet

--- core/Interactable.gd
+++ core/Interactable.gd
@@ -7,15 +7,15 @@ var can_interact: bool = false
 @export var is_blocked: bool = false
 @onready var info_label: Label = $Info
 		
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed('interact'):
 		if can_interact && !is_blocked:
 			interaction.emit()
 
-func _on_body_entered(body: Player) -> void:
+func _on_body_entered(_body: Player) -> void:
 	print_debug("Player can interact")
 	can_interact = true
 	on_interaction_area.emit(true)
 	
-func _on_body_exited(body: Player) -> void:
+func _on_body_exited(_body: Player) -> void:
 	on_interaction_area.emit(false)
