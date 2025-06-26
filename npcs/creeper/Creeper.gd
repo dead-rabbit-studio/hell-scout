774d9dfda484e4aaf1c49a8b9ef7ab92b9038311
casti <felipecastilhos@gmail.com>
2025-06-26 20:05:09 -0300

    refact: improve creeper follow function

--- npcs/creeper/Creeper.gd
+++ npcs/creeper/Creeper.gd
@@ -12,9 +12,12 @@ const NODE_NAME = "Creeper"
 @onready var health: Health = $Health
 
 var _player_position: Vector2
+var _player_in_sight: Node2D 
 var _player_los: bool = false
 
-# Can be called to kill a creeper by the game keeper
+func take_damage(damage_taken: float):
+	health.damage(damage_taken)
+
 func die() -> void:
 	queue_free()
 	print_debug("a creeper has died")
@@ -25,39 +28,34 @@ func _ready() -> void:
 	health.current = max_health
 	print_debug("new creeper health:" + str(health.current))
 	
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if health.is_alive:
 		line_of_sight_area.radius = line_of_sight_size
+		_follow_player(delta)
 
-func _physics_process(delta: float) -> void:
-	_follow_player(delta)
-	
 func _follow_player(delta: float) -> void: 
 	const personal_space_area = 30
-	if _player_los:
-		var distance_from_player_vector: Vector2 = _player_position - global_position
+	if _player_in_sight:
+		var distance_from_player_vector: Vector2 = _player_in_sight.global_position - global_position
 		var direction: Vector2 = distance_from_player_vector.normalized()
 		var distance_lenght: float = abs(distance_from_player_vector.length())
 		
 		var movement: Vector2 = direction * (speed * delta)
 		
+		print_debug("Creeper: Player in line of sight" + str(_player_position) + " distance: " + str(distance_lenght))
 		if distance_lenght > personal_space_area:
 			move_and_collide(movement)		
-
-func _on_player_player_position_update(new_player_position: Vector2) -> void:
-	_player_position = new_player_position
-	
-func take_damage(damage_taken: float):
-	health.damage(damage_taken)
 
 func _on_health_depleted() -> void:
 	die()
 
 func _on_engage_area_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		_player_los = true	
+		_player_los = true
+		_player_in_sight = body
 		_player_position = body.global_position
 
 func _on_engage_area_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D:
+		_player_in_sight = null
 		_player_los = false
