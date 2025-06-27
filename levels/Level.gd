4986fb19754f07d305490c40527a9e3a33dd1aa8
casti <felipecastilhos@gmail.com>
2025-06-26 21:07:27 -0300

    refact: remove player_position_update signal

--- levels/Level.gd
+++ levels/Level.gd
@@ -29,12 +29,10 @@ func _game_over() -> void:
 	print_debug("level finished")
 	level_status_change.emit(LevelStatus.LOST)
 	mob_spawner.stop()
-	#_kill_all_mobs()
 	
 func _spawn_creeper() -> void:
 	var creeper: Creeper = CreeperClass.instantiate()
 	creeper.position = _get_spawn_position_inside_area(player.global_position, 50) 
-	player.player_position_update.connect(creeper._on_player_player_position_update)
 	add_child(creeper)
 	
 func _get_spawn_position_inside_area(player_position:Vector2, radius: float) -> Vector2:
